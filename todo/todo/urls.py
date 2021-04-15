1188b56b48ed3ce5a63e05d8b29e1c653d4d7605
Родители <roditeli@iMac-iMac.local>
2021-04-15 18:09:43 +0600

    added in todo admin.py and models.py and urls.py

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, second, third
 from django.conf import settings
-from dlango.conf.urls.static import static
+from django.conf.urls.static import static
 
 
 urlpatterns = [
