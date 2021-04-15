d908983c9224666495998a97e32ce9bcf7ea94b4
Родители <roditeli@iMac-iMac.local>
2021-04-15 09:59:43 +0600

    set static and media

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, second, third
+from django.conf import settings
+from dlango.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,5 @@ urlpatterns = [
     path('test/', test, name="test"),
     path('test2', second),
     path('test3', third)
-]
+]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)\
+    + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
