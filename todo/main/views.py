3cb47b7f5f82e44fb586bc2459f66a2af7688221
Родители <roditeli@iMac-iMac.local>
2021-04-16 14:22:00 +0600

    date base in test.html

--- todo/main/views.py
+++ todo/main/views.py
@@ -7,7 +7,7 @@ def homepage (request):
 
 def test(request):
     todo_list = ToDo.objects.all()
-    return render(request, "test.html", {"todo_list":todo_list})
+    return render(request, "test.html", {"todo_list": todo_list})
 
 def second(request):
     return HttpResponse("uraa test2")
